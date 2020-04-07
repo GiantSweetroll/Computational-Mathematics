fb0ab658df4633cbdb1b183f2a3a2e435d4ab1a0
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-04-07 14:33:54 +0700

    main file for the midterms exams later

---
+++ mid_exam/main.py
@@ -0,0 +1,5 @@
+import mid_exam.euler_method_calculator as euler
+import mid_exam.numerical_integration as ni
+import mid_exam.runge_kutta_fourth_order_calculator as rk
+import mid_exam.taylor_series_calculator as ty
+import sympy as sp
