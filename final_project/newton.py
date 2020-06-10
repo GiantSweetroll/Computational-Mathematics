321f04c6b7bab6d06cc719d4aed563a658500017
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-06-10 14:21:13 +0700

    Converted steps to methods

--- final_project/newton.py
+++ final_project/newton.py
@@ -11,70 +11,88 @@ xy_values.append([20, 517.35])
 xy_values.append([22.5, 602.97])
 
 #Initialize divided difference table
-table = []
-
-for _ in range(len(xy_values)):
-    temp = []
-    for _ in range(len(xy_values) + 1):
-        temp.append(-1)
-    table.append(temp)
+def init_table():
+    table = []
+    
+    for _ in range(len(xy_values)):
+        temp = []
+        for _ in range(len(xy_values) + 1):
+            temp.append(-1)
+        table.append(temp)
         
-#Insert x and y values to table
-for i in range(len(xy_values)):
-    table[i][0] = xy_values[i][0]
-    table[i][1] = xy_values[i][1]
+    #Insert x and y values to table
+    for i in range(len(xy_values)):
+        table[i][0] = xy_values[i][0]
+        table[i][1] = xy_values[i][1]
+    
+    return table
 
 #Do the divided difference table
-y_bound = 1
-for col in range(2, len(table[0])):
-    for row in range(y_bound, len(table)):
-        try:
-            delta = (table[row][col-1] - table[y_bound-1][col-1]) / (table[row][0] - table[y_bound-1][0])
-        except:
-            delta = 0
-        #print(table[row][col-1], '-', table[y_bound-1][col-1], "divide", table[row][0], '-', table[y_bound-1][0], '=', delta)
-        table[row][col] = delta
-    y_bound += 1
+def compute_table(table):
+    y_bound = 1
+    for col in range(2, len(table[0])):
+        for row in range(y_bound, len(table)):
+            try:
+                delta = (table[row][col-1] - table[y_bound-1][col-1]) / (table[row][0] - table[y_bound-1][0])
+            except:
+                delta = 0
+            #print(table[row][col-1], '-', table[y_bound-1][col-1], "divide", table[row][0], '-', table[y_bound-1][0], '=', delta)
+            table[row][col] = delta
+        y_bound += 1
 
 #Get an values
-an = []
-col = 1
-for row in range(0, len(table)):
-    an.append(table[row][col])
-    col += 1
+def get_an_values(table):
+    an = []
+    col = 1
+    for row in range(0, len(table)):
+        an.append(table[row][col])
+        col += 1
+    return an
     
 #create function and simplify it
-x = sp.Symbol('x')
-func = 0
-for a in range(len(an)):
-    product = an[a]
-    for i in range(a):
-        product *= (x - xy_values[i][0])
-    func += product
+def get_equation(an, x_symb):
+    func = 0
+    for a in range(len(an)):
+        product = an[a]
+        for i in range(a):
+            product *= (x_symb - xy_values[i][0])
+        func += product
+    
+    func = sp.simplify(func)
+    
+    print(func)
+    
+    return func
 
-func = sp.simplify(func)
-print(func)
+#putting it together
+x = sp.Symbol('x')
+table = init_table()
+compute_table(table)
+an = get_an_values(table)
+func = get_equation(an, x)
 
 #Plotting
-#split n and y
-x_values = []
-y_values = []
-
-for i in range(len(xy_values)):
-    x_values.append(xy_values[i][0])
-    y_values.append(xy_values[i][1])
-
-#Generate x and y
-new_x_values = []
-new_y_values = []
-for i in range(int(min(x_values) * 100), int(max(x_values) * 100), 1):
-    new_x_values.append(i/100)
-    new_y_values.append(func.evalf(subs={x:i/100}))
+def graph_newton(xy_values, func, x_symb):
+    #split n and y
+    x_values = []
+    y_values = []
     
-plt.plot(x_values, y_values, 'o', label='data')
-plt.plot(new_x_values, new_y_values, '-', label='equation')
-plt.legend()
-plt.xlabel("X")
-plt.ylabel("Y")
-
-plt.show()
+    for i in range(len(xy_values)):
+        x_values.append(xy_values[i][0])
+        y_values.append(xy_values[i][1])
+    
+    #Generate x and y
+    new_x_values = []
+    new_y_values = []
+    for i in range(int(min(x_values) * 100), int(max(x_values) * 100), 1):
+        new_x_values.append(i/100)
+        new_y_values.append(func.evalf(subs={x_symb:i/100}))
+        
+    plt.plot(x_values, y_values, 'o', label='data')
+    plt.plot(new_x_values, new_y_values, '-', label='equation')
+    plt.legend()
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    
+    plt.show()
+graph_newton(xy_values, func, x)
